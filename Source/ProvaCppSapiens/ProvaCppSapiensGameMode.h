707b2d69a230efd7d680c752e692d7976b533064
Felippe Malagueta Fernandes <felippesud@gmail.com>
2019-12-02 15:57:57 -0400

    Primeiros Arquivos
    Upload inicial com o templet de terceira pessoa, sem codificação

---
+++ Source/ProvaCppSapiens/ProvaCppSapiensGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProvaCppSapiensGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProvaCppSapiensGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProvaCppSapiensGameMode();
+};
+
+
+
